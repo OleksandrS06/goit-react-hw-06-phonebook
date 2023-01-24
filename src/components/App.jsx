cd379e323674562becd91fcaa909dd30e445ab79
OleksandrS06 <112249299+OleksandrS06@users.noreply.github.com>
2023-01-24 23:01:00 +0100

    app coorrected

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
 
 import Form from './ContactAddForm/ContactAddForm';
 import ContactsList from './ContactsList/ContactsList';
