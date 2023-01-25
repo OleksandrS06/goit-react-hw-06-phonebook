7696b5cb9df7c0f8d90c869e8d13ae2ec951ebc4
OleksandrS06 <112249299+OleksandrS06@users.noreply.github.com>
2023-01-25 11:22:18 +0100

    app filter&contacsList render condition added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import Form from './ContactAddForm/ContactAddForm';
 import ContactsList from './ContactsList/ContactsList';
 import Filter from './Filter';
+import { getContactsValue } from 'redux/contactsSlice/contactsSlice';
 
 const App = () => {
+  let contacts = useSelector(getContactsValue);
   return (
     <div className="wrapper">
       <h1 className="heading">Phonebook</h1>
       <Form />
-      <h2>Find Contacts</h2>
-      <Filter />
-      <ContactsList />
+      {contacts.length > 0 ? (
+        <>
+          <h2>Find Contacts</h2>
+          <Filter />
+          <ContactsList />
+        </>
+      ) : (
+        <p>There are no any contacts at the moment</p>
+      )}
     </div>
   );
 };
